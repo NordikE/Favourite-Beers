12c47ab8394dfd6716f48b8c9cdff2f16a332182
NordikE <erik.b10.kodehode@gmail.com>
2024-03-11 10:38:28 +0100

    Folder structure

--- src/BeerInfo.js
+++ src/BeerInfo.js
@@ -1,1 +1,55 @@
-.
+const beerInfo = [
+  {
+    id: 1,
+    name: "Aegir Minus",
+    image: "./assets/AegirMinus.jpg",
+  },
+
+  {
+    id: 2,
+    name: "Amundsen Hazy IPA",
+    image: "./assets/AmundsenHazy.jpg",
+  },
+
+  {
+    id: 3,
+    name: "Althletic Run Wild IPA",
+    image: "./assets/AthleticRunWild.jpg",
+  },
+
+  {
+    id: 4,
+    name: "BrewDog Lost AF lager",
+    image: "./assets/BrewdogLostAF.jpg",
+  },
+
+  {
+    id: 5,
+    name: "BrewDog Nanny State Hoppy Ale",
+    image: "./assets/BrewdogNannyState.jpg",
+  },
+
+  {
+    id: 6,
+    name: "Brooklyn Special Effects",
+    image: "./assets/BrooklynSpecialEffects.jpg",
+  },
+
+  {
+    id: 7,
+    name: "Brulo MAngo Guava IPA",
+    image: "./assets/AegirMinus.jpg",
+  },
+
+  {
+    id: ,
+    name: "Aegir Minus",
+    image: "./assets/AegirMinus.jpg",
+  },
+
+  {
+    id: ,
+    name: "Aegir Minus",
+    image: "./assets/AegirMinus.jpg",
+  },
+];
