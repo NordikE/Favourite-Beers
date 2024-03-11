ac55d04dc7aced3cfb125011bf4e8961f3d7cf6a
NordikE <erik.b10.kodehode@gmail.com>
2024-03-11 10:51:29 +0100

    .

--- public/Components/BeerInfo.js
+++ public/Components/BeerInfo.js
@@ -1,1 +1,109 @@
-.
+const beerInfo = [
+  {
+    id: 1,
+    name: "Ægir Minus",
+    image: "./BeerInfoImages/AegirMinus.jpg",
+  },
+
+  {
+    id: 2,
+    name: "Amundsen Hazy IPA",
+    image: "./BeerInfoImages/AmundsenHazy.jpg",
+  },
+
+  {
+    id: 3,
+    name: "Athletic Run Wild Hazy IPA",
+    image: "./BeerInfoImages/AthleticRunWild.jpg",
+  },
+
+  {
+    id: 4,
+    name: "BrewDog Lost AF Lager",
+    image: "./BeerInfoImages/BrewdogLostAF.jpg",
+  },
+
+  {
+    id: 5,
+    name: "BrewDog Nanny State Hoppy Ale",
+    image: "./BeerInfoImages/BrewdogNannyState.jpg",
+  },
+
+  {
+    id: 6,
+    name: "Brooklyn Special Effects Hoppy Lager",
+    image: "./BeerInfoImages/BrooklynSpecialEffects.jpg",
+  },
+
+  {
+    id: 7,
+    name: "Brulo Mango Guava IPA",
+    image: "./BeerInfoImages/BruloMango.jpg",
+  },
+
+  {
+    id: 8,
+    name: "Erdinger Blue Weissbrau",
+    image: "./BeerInfoImages/ErdingerBlue.jpg",
+  },
+
+  {
+    id: 9,
+    name: "Nøgne Ø Julefri",
+    image: "./BeerInfoImages/NoegneOeJulefri.jpg",
+  },
+
+  {
+    id: 10,
+    name: "Nøgne Ø Svart Hvit",
+    image: "",
+  },
+
+  {
+    id: 11,
+    name: "Lervig No Worries IPA",
+    image: "./BeerInfoImages/NoWorries.jpg",
+  },
+
+  {
+    id: 12,
+    name: "Lervig No Worries Christmas Ale",
+    image: "./BeerInfoImages/NoWorriesChristmas.jpg",
+  },
+
+  {
+    id: 13,
+    name: "Paulaner Blue Weiss Beer",
+    image: "./BeerInfoImages/PaulanderWeissBlue.jpg",
+  },
+
+  {
+    id: 13,
+    name: "Paulaner Blue Hefe Weiss Beer",
+    image: "./BeerInfoImages/PaulanderWeissBlue.jpg",
+  },
+
+  {
+    id: 12,
+    name: "Lervig No Worries Christmas Ale",
+    image: "./BeerInfoImages/NoWorriesChristmas.jpg",
+  },
+
+  {
+    id: 12,
+    name: "Lervig No Worries Christmas Ale",
+    image: "./BeerInfoImages/NoWorriesChristmas.jpg",
+  },
+
+  {
+    id: 12,
+    name: "Lervig No Worries Christmas Ale",
+    image: "./BeerInfoImages/NoWorriesChristmas.jpg",
+  },
+
+  {
+    id: 12,
+    name: "Lervig No Worries Christmas Ale",
+    image: "./BeerInfoImages/NoWorriesChristmas.jpg",
+  },
+];
