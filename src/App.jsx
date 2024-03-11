bed453dc113781b833d9e68c54ebc813a7e6fd63
NordikE <erik.b10.kodehode@gmail.com>
2024-03-11 10:09:34 +0100

    Folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import beer1 from "./assets/13643502-1.jpg";
 import userData from "./static_data/users.json";
 
 function App() {
+  console.log(userData);
   return (
     <>
       <img src={beer1} alt="First favourite beer" />;
