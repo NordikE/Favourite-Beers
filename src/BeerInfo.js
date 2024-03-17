58877f75eaf3976e199ac97a254112490c4c8de4
NordikE <erik.b10.kodehode@gmail.com>
2024-03-17 16:06:03 +0100

    File paths

--- src/BeerInfo.js
+++ src/BeerInfo.js
@@ -2,71 +2,71 @@ export const BeerInfo = [
   {
     id: 1,
     name: "Aegir Minus",
-    image: "../src/assets/Components/AegirMinus.jpg",
+    image: "./assets/Components/AegirMinus.jpg",
   },
 
   {
     id: 2,
     name: "Amundsen Hazy IPA",
-    image: "../src/assets/Components/AmundsenHazy.jpg",
+    image: "./assets/Components/AmundsenHazy.jpg",
   },
 
   {
     id: 3,
     name: "Althletic Run Wild IPA",
-    image: "../src/assets/Components/AthleticRunWild.jpg",
+    image: "./assets/Components/AthleticRunWild.jpg",
   },
 
   {
     id: 4,
     name: "BrewDog Lost AF lager",
-    image: "../src/assets/Components/BrewdogLostAF.jpg",
+    image: "./assets/Components/BrewdogLostAF.jpg",
   },
 
   {
     id: 5,
     name: "BrewDog Nanny State Hoppy Ale",
-    image: "../src/assets/Components/BrewdogNannyState.jpg",
+    image: "./assets/Components/BrewdogNannyState.jpg",
   },
 
   {
     id: 6,
     name: "Brooklyn Special Effects",
-    image: "../src/assets/Components/BrooklynSpecialEffects.jpg",
+    image: "./assets/Components/BrooklynSpecialEffects.jpg",
   },
 
   {
     id: 7,
     name: "Brulo MAngo Guava IPA",
-    image: "../src/assets/Components/BruloMango.jpg",
+    image: "./assets/Components/BruloMango.jpg",
   },
 
   {
     id: 8,
     name: "Erdinger Blue Weiss",
-    image: "../src/assets/Components/ErdingerBlue.jpg",
+    image: "./assets/Components/ErdingerBlue.jpg",
   },
 
   {
     id: 9,
     name: "Nøgne Ø Julefri",
-    image: "../src/assets/Components/NoegneOeJulefri.jpg",
+    image: "./assets/Components/NoegneOeJulefri.jpg",
   },
   {
     id: 10,
     name: "Nøgne Ø Svart/ Hvit",
-    image: "../src/assets/Components/NoegneOeSvartHvit.jpg",
+    image: "./assets/Components/NoegneOeSvartHvit.jpg",
   },
 
   {
     id: 11,
     name: "Lervig No Worries",
-    image: "../src/assets/Components/NoWorries.jpg",
+    image: "./assets/Components/NoWorries.jpg",
   },
 
   {
     id: 12,
     name: "Lervig No Worries Driving Home For Christmas",
-    image: "../src/assets/Components/NoWorriesChristmas.jpg",
+    image: "./assets/Components/NoWorriesChristmas.jpg",
   },
 ];
