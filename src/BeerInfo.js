3e0ab0e392cd488643aa62908622458cf67486f7
NordikE <erik.b10.kodehode@gmail.com>
2024-03-17 16:30:28 +0100

    File paths

--- src/BeerInfo.js
+++ src/BeerInfo.js
@@ -8,7 +8,7 @@ export const BeerInfo = [
   {
     id: 2,
     name: "Amundsen Hazy IPA",
-    image: "../src/assets/Components/AmundsenHazy.jpg",
+    image: "./viteproject/src/assets/Components/AmundsenHazy.jpg",
   },
 
   {
@@ -20,53 +20,53 @@ export const BeerInfo = [
   {
     id: 4,
     name: "BrewDog Lost AF lager",
-    image: "./assets/Components/BrewdogLostAF.jpg",
+    image: "../src/assets/Components/BrewdogLostAF.jpg",
   },
 
   {
     id: 5,
     name: "BrewDog Nanny State Hoppy Ale",
-    image: "./assets/Components/BrewdogNannyState.jpg",
+    image: "../src/assets/Components/BrewdogNannyState.jpg",
   },
 
   {
     id: 6,
     name: "Brooklyn Special Effects",
-    image: "./assets/Components/BrooklynSpecialEffects.jpg",
+    image: "../src/assets/Components/BrooklynSpecialEffects.jpg",
   },
 
   {
     id: 7,
     name: "Brulo MAngo Guava IPA",
-    image: "./assets/Components/BruloMango.jpg",
+    image: "../src/assets/Components/BruloMango.jpg",
   },
 
   {
     id: 8,
     name: "Erdinger Blue Weiss",
-    image: "./assets/Components/ErdingerBlue.jpg",
+    image: "../src/assets/Components/ErdingerBlue.jpg",
   },
 
   {
     id: 9,
     name: "Nøgne Ø Julefri",
-    image: "./assets/Components/NoegneOeJulefri.jpg",
+    image: "../src/assets/Components/NoegneOeJulefri.jpg",
   },
   {
     id: 10,
     name: "Nøgne Ø Svart/ Hvit",
-    image: "./assets/Components/NoegneOeSvartHvit.jpg",
+    image: "../src/assets/Components/NoegneOeSvartHvit.jpg",
   },
 
   {
     id: 11,
     name: "Lervig No Worries",
-    image: "./assets/Components/NoWorries.jpg",
+    image: "../src/assets/Components/NoWorries.jpg",
   },
 
   {
     id: 12,
     name: "Lervig No Worries Driving Home For Christmas",
-    image: "./assets/Components/NoWorriesChristmas.jpg",
+    image: "../src/assets/Components/NoWorriesChristmas.jpg",
   },
 ];
