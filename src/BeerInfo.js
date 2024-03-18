0d760b36cb81cae43f0aec4d4961ab7307387663
NordikE <erik.b10.kodehode@gmail.com>
2024-03-18 09:06:09 +0100

    Paths

--- src/BeerInfo.js
+++ src/BeerInfo.js
@@ -8,65 +8,65 @@ export const BeerInfo = [
   {
     id: 2,
     name: "Amundsen Hazy IPA",
-    image: "./viteproject/src/assets/Components/AmundsenHazy.jpg",
+    image: "../public/assets/AmundsenHazy.jpg",
   },
 
   {
     id: 3,
     name: "Althletic Run Wild IPA",
-    image: "./assets/Components/AthleticRunWild.jpg",
+    image: "../public/assets/AthleticRunWild.jpg",
   },
 
   {
     id: 4,
     name: "BrewDog Lost AF lager",
-    image: "./src/assets/Components/BrewdogLostAF.jpg",
+    image: "../public/assets/BrewdogLostAF.jpg",
   },
 
   {
     id: 5,
     name: "BrewDog Nanny State Hoppy Ale",
-    image: "../src/assets/Components/BrewdogNannyState.jpg",
+    image: ".../public/assets/BrewdogNannyState.jpg",
   },
 
   {
     id: 6,
     name: "Brooklyn Special Effects",
-    image: "../src/assets/Components/BrooklynSpecialEffects.jpg",
+    image: ".../public/assets/BrooklynSpecialEffects.jpg",
   },
 
   {
     id: 7,
     name: "Brulo MAngo Guava IPA",
-    image: "../src/assets/Components/BruloMango.jpg",
+    image: ".../public/assets/BruloMango.jpg",
   },
 
   {
     id: 8,
     name: "Erdinger Blue Weiss",
-    image: "../src/assets/Components/ErdingerBlue.jpg",
+    image: ".../public/assets/ErdingerBlue.jpg",
   },
 
   {
     id: 9,
     name: "Nøgne Ø Julefri",
-    image: "../src/assets/Components/NoegneOeJulefri.jpg",
+    image: ".../public/assets/NoegneOeJulefri.jpg",
   },
   {
     id: 10,
     name: "Nøgne Ø Svart/ Hvit",
-    image: "../src/assets/Components/NoegneOeSvartHvit.jpg",
+    image: ".../public/assets/NoegneOeSvartHvit.jpg",
   },
 
   {
     id: 11,
     name: "Lervig No Worries",
-    image: "../src/assets/Components/NoWorries.jpg",
+    image: ".../public/assets/NoWorries.jpg",
   },
 
   {
     id: 12,
     name: "Lervig No Worries Driving Home For Christmas",
-    image: "../src/assets/Components/NoWorriesChristmas.jpg",
+    image: ".../public/assets/NoWorriesChristmas.jpg",
   },
 ];
